e99563fa7d763126eea3d48cae8976f541b20903
DominikHracek <domca.hracek@gmail.com>
2023-05-17 13:48:18 +0200

    Python program, which is working with files

---
+++ soubory.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+#1
+def otevreni_souboru(file_path):
+    path_to_file = Path.open(file_path)
+    file_content = path_to_file.read()
+    return file_content
+
+file_path = input("Full path of the file: ")
+file_content_path = input("Full path of the content of the file: ")
+file_content = Path(file_content_path).open("w")
+file_content.write(otevreni_souboru(file_path))
+
+#2
+lines = []
+
+passwd_path = input("Full path of the content of the file: ")
+passwd = Path(passwd_path).open("w")
+passwd.write("")
+passwd = Path(passwd_path).open("a")
+file_path = Path.open("/etc/passwd")
+for line in file_path.readlines():
+    lines = line.split(":")
+    #3
+    passwd.write(f"Username: {lines[0]} and bash: {lines[-1]}")
+
+#4
+dir = input("Full path of the directory to show: ")
+directory = Path(dir)
+for file in directory.iterdir():
+    if file.is_file():
+        print(f"{file} is a file")
+    elif file.is_dir():
+        print(f"{file} is a directory")
+    else:
+        print("I don't know what that is")
