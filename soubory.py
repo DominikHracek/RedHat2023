6c4796a02d5cf28ac30c13a552bc89e03ad327c2
DominikHracek <domca.hracek@gmail.com>
2023-05-17 13:58:02 +0200

    Some changes were made

--- soubory.py
+++ soubory.py
@@ -13,24 +13,25 @@ file_content.write(otevreni_souboru(file_path))
 
 #2
 lines = []
-
-passwd_path = input("Full path of the content of the file: ")
-passwd = Path(passwd_path).open("w")
-passwd.write("")
-passwd = Path(passwd_path).open("a")
-file_path = Path.open("/etc/passwd")
-for line in file_path.readlines():
-    lines = line.split(":")
-    #3
-    passwd.write(f"Username: {lines[0]} and bash: {lines[-1]}")
+def read_and_write_passwd():
+    passwd_path = input("Full path of the content of the file: ")
+    passwd = Path(passwd_path).open("w")
+    passwd.write("")
+    passwd = Path(passwd_path).open("a")
+    file_path = Path.open("/etc/passwd")
+    for line in file_path.readlines():
+        lines = line.split(":")
+        #3
+        passwd.write(f"Username: {lines[0]} and bash: {lines[-1]}")
 
 #4
-dir = input("Full path of the directory to show: ")
-directory = Path(dir)
-for file in directory.iterdir():
-    if file.is_file():
-        print(f"{file} is a file")
-    elif file.is_dir():
-        print(f"{file} is a directory")
-    else:
-        print("I don't know what that is")
+check_directory():
+    dir = input("Full path of the directory to show: ")
+    directory = Path(dir)
+    for file in directory.iterdir():
+        if file.is_file():
+            print(f"{file} is a file")
+        elif file.is_dir():
+            print(f"{file} is a directory")
+        else:
+            print("I don't know what that is")
