c4b0da07165491be1d1e8f62592c95e2bd5e61bc
DominikHracek <domca.hracek@gmail.com>
2023-05-24 10:43:05 +0200

    Python program calculating perimeter and area of a square, factorial and makes a sum of all numbers in a dictionary

--- slovniky_a_obvod_obsah_faktorial.py
+++ slovniky_a_obvod_obsah_faktorial.py
@@ -1,24 +1,27 @@
 def obvod(strana_a):
     print(f"Obvod ctverce je: {4*strana_a}")
+    return 4*strana_a
 
 def obsah(strana_a):
     print(f"Obsah ctverce je: {strana_a**2}")
+    return strana_a**2
 
 def faktorial(cislo_na_faktorial):
     faktorial = 1
     for i in range(1, cislo_na_faktorial+1):
         faktorial *= i
     print(f"Faktorial čísla {cislo_na_faktorial} je: {faktorial}")
+    return faktorial
 
-def main(parametr):
+"""def main(parametr):
     obvod(parametr)
     obsah(parametr)
     faktorial(parametr)
 
 parametr = int(input("S jakým číslem chceš počítat: "))
 main(parametr)
-
-slovnik = {}
+"""
+"""slovnik = {}
 
 pocet_cisel_ve_slovniku = int(input("Kolik čísel ve slovníku: "))
 for i in range(pocet_cisel_ve_slovniku):
@@ -29,4 +32,4 @@ for i in range(pocet_cisel_ve_slovniku):
 soucet = 0
 for key, value in slovnik.items():
     soucet += value
-print(soucet)
+print(soucet)"""
